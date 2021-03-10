dba7834a3a79c9833d739435d1ed661100ee0d79
Aymie <aymie.walid.chalouhy@gmail.com>
2021-03-10 23:15:53 +0200

    step5 update

--- javascript-basics-01/step_5.js
+++ javascript-basics-01/step_5.js
@@ -1,6 +1,6 @@
-/*
-  var n1 = document.getElementById('enter the fisrt number').value;
-  var n2 = document.getElementById('enter the second number').value; 
-  var res = n1 * n2 ;
-   alert(n1 + " * " + n2 + " = " + res);
- */
+function multiplying() {
+  var fn = document.getElementById('first_number').value;
+ var sn = document.getElementById('second_number').value;  
+ var res = fn * sn ;
+ alert(fn + " * " + sn + " = " + res);
+}
