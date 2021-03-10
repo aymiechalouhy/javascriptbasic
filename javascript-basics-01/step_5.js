09b8f095b4145e21ca69d3746aac577770345528
Aymie <aymie.walid.chalouhy@gmail.com>
2021-03-10 14:08:57 +0200

    Step 5

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,6 @@
+function multiplying() {
+    var n1 = document.getElementById('enter the fisrt number').value;
+   var n2 = document.getElementById('enter the second number').value; 
+   var res = n1 * n2 ;
+   alert(n1 + " * " + n2 + " = " + res);
+ }
