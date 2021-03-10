bb104a808f966d160b6fb0b1d9e89054a0f08f2f
Aymie <aymie.walid.chalouhy@gmail.com>
2021-03-10 13:51:23 +0200

    Step 4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,9 @@
+
+    var name = document.getElementById('Name').value;
+    var surname = document.getElementById(' Surname').value;  
+    var city = document.getElementById(' City').value;
+
+
+   alert("\n Name : "+ name+
+    "\n Surname : "+ surname+
+    "\n City : "+city);
