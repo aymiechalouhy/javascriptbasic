9876d2d698df98487b3b3adcf54fb1fa4da30641
Aymie <aymie.walid.chalouhy@gmail.com>
2021-03-14 00:05:39 +0200

    step 2

--- javascript-basics-03/step 2/step_2.js
+++ javascript-basics-03/step 2/step_2.js
@@ -1,13 +1,13 @@
 var show = document.querySelector("#show");
 var hide = document.querySelector("#hide");
-var text1 = document.querySelector("p");
+var tt = document.querySelector("p");
 
 const change = (id)=>{
     if(id == "show"){
-        text1.style.display = 'block';
+        tt.style.display = 'block';
     }
     if(id == "hide"){
-        text1.style.display = 'none';
+        tt.style.display = 'none';
     }
 };
 
